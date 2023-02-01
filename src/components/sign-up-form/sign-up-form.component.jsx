df02b952a7fc069c7b05c27db046f3a0b4857d79
jpalencia77 <jpalencia@ucol.mx>
2023-02-01 14:05:59 -0600

    auth listener

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,9 @@
 import { async } from "@firebase/util";
-import { useState, useContext } from "react";
+import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import './sign-up-form.styles.scss'
-import { UserContext } from "../../contexts/user.context";
+
 
 import Button from "../button/button.component";
 const defaultFormFields = {
@@ -18,8 +18,7 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
-    const {setCurrentUser} = useContext(UserContext)
-
+    
     const resetFormFields = () => {
         setFormFields(defaultFormFields)
     }
@@ -35,7 +34,7 @@ const SignUpForm = () => {
         try {
             const { user } = await createAuthUserWithEmailAndPassword(email, password)
 
-            setCurrentUser(user);
+            
             await createUserDocumentFromAuth(user, { displayName })
             resetFormFields()
 
