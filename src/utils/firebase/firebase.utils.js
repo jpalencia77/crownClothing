dc667327c0777006fc6e01fb9808e2ae4ac116af
jpalencia77 <jpalencia@ucol.mx>
2023-02-09 00:42:45 -0600

    add collection and documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,17 +7,18 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-onAuthStateChanged
+  onAuthStateChanged
+
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyD8Q1ZycYYm9NrO92I0Ucu7IwS2FBgQKdk",
+  authDomain: "crwn-clothing-db-3216f.firebaseapp.com",
+  projectId: "crwn-clothing-db-3216f",
+  storageBucket: "crwn-clothing-db-3216f.appspot.com",
+  messagingSenderId: "1030473490744",
+  appId: "1:1030473490744:web:5353991c7a37c00e1ae1cd"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -35,6 +36,25 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+
+export const addCollectionAndDocuments = async (
+  collectionKey, 
+  objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+
+    batch.set(docRef, object)
+  })
+
+  await batch.commit();
+  console.log('done')
+
+
+}
 
 export const createUserDocumentFromAuth = async (
   userAuth,
@@ -80,7 +100,7 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 
-export const onAuthStateChangedListener = (callback) => 
-onAuthStateChanged(auth, callback);
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
 
 
