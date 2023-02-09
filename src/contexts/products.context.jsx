39fc90415ae0a3f996557bb3a67a7501e5f9726e
jpalencia77 <jpalencia@ucol.mx>
2023-02-09 14:01:35 -0600

    get products and categories from firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,27 @@
+import { async } from '@firebase/util';
 import { createContext, useState, useEffect } from 'react';
 
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+
+    const getCategoriesMap = async () => {
+    const categoryMap =  await  getCategoriesAndDocuments()
+    console.log(categoryMap)
+    }
+    
+    getCategoriesMap()
+  }, [])
+  
   
 
   const value = { products };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,16 @@ import {
   onAuthStateChanged
 
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from 'firebase/firestore';
+import { 
+  getFirestore, 
+  doc, 
+  getDoc, 
+  setDoc, 
+  collection, 
+  writeBatch, 
+  query,
+  getDocs
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD8Q1ZycYYm9NrO92I0Ucu7IwS2FBgQKdk",
@@ -55,6 +64,28 @@ export const addCollectionAndDocuments = async (
 
 
 }
+
+
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories' );
+
+  const q = query(collectionRef);
+
+
+  const querySnapshot = await getDocs(q)
+
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+   const  {title, items} = docSnapshot.data();
+   acc[title.toLowerCase()] = items;
+   return acc;
+  }, {})
+  
+
+  return categoryMap
+
+}
+
 
 export const createUserDocumentFromAuth = async (
   userAuth,
