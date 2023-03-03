e7be525b0c1f69469d53f774c99c485a53a33312
jpalencia77 <jpalencia@ucol.mx>
2023-03-02 18:33:18 -0600

    writing middleware

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,7 @@
-export const selectCategoriesMap = (state) => 
-state.categories.categories.reduce((acc, category) => {
+export const selectCategoriesMap = (state) => {
+return state.categories.categories.reduce((acc, category) => {
     const  {title, items} = category;
     acc[title.toLowerCase()] = items;
     return acc;
    }, {})
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,20 @@
 import {compose, createStore, applyMiddleware} from 'redux'
 
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 
 import { rootReducer } from './root-reducer'
 
-const middleWares = [logger]
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action);
+    }
+
+    next(action)
+}
+
+const middleWares = [loggerMiddleware]
 
 const composeEnhancers = compose(applyMiddleware(...middleWares))
 export const store = createStore(rootReducer, undefined, composeEnhancers)
