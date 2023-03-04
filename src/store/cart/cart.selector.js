3f5e33f41f07df4be2d8661cbdffea952e89b7d6
jpalencia77 <jpalencia@ucol.mx>
2023-03-04 13:32:56 -0600

    cart action reselect 1

---
+++ src/store/cart/cart.selector.js
@@ -0,0 +1,12 @@
+import {createSelector} from 'reselect'
+
+
+const newCartCount = cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity,
+    0
+  );
+
+  const newCartTotal = cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity * cartItem.price,
+    0
+  );
