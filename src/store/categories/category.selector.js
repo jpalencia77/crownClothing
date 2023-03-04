16618301f7e9c228355125bfed4a64b1f86577fb
jpalencia77 <jpalencia@ucol.mx>
2023-03-04 12:40:56 -0600

    category selector with reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,4 +1,4 @@
-import {createSelector} from 'reselect';
+import { createSelector } from 'reselect';
 
 const selectCartegoryReducer = (state) => state.categories;
 
@@ -9,11 +9,11 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => 
-        
-       categories.reduce((acc, category) => {
-        const  {title, items} = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-       }, {})
+    (categories) =>
+
+        categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
 )
