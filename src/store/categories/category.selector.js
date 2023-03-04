98b5745fd27495eb0f27469af8cad25028a32f22
jpalencia77 <jpalencia@ucol.mx>
2023-03-04 12:40:30 -0600

    category selector reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,19 @@
-export const selectCategoriesMap = (state) => {
-return state.categories.categories.reduce((acc, category) => {
-    const  {title, items} = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-   }, {})
-}
+import {createSelector} from 'reselect';
+
+const selectCartegoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCartegoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => 
+        
+       categories.reduce((acc, category) => {
+        const  {title, items} = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+       }, {})
+)
