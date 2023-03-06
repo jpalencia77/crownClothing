689cdc706607abbf7326281b91c60c1491f7a959
jpalencia77 <jpalencia@ucol.mx>
2023-03-06 14:33:05 -0600

    cart dropdown select

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,16 +1,23 @@
 import { useContext } from 'react'
-//import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
-import {CartIconContainer, ItemCount, ShoppingIcon} from './cart-icon.styles'
-import { CartContext } from '../../contexts/cart.context'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector'
+import { setIsCartOpen } from '../../store/cart/cart.action'
+
+import { CartIconContainer, ItemCount, ShoppingIcon } from './cart-icon.styles'
+
 
 const CartIcon = () => {
 
-    const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext)
-    
-    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
-    return(
+    const dispatch = useDispatch()
+
+    const cartCount = useSelector(selectCartCount)
+    const isCartOpen = useSelector(selectIsCartOpen)
+
+    const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen))
+    return (
         <CartIconContainer className='cart-icon-container' onClick={toggleIsCartOpen}>
-            <ShoppingIcon className='shopping-icon'/>
+            <ShoppingIcon className='shopping-icon' />
             <ItemCount>{cartCount}</ItemCount>
         </CartIconContainer>
     )
--- src/index.js
+++ src/index.js
@@ -6,26 +6,15 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store/store';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from './contexts/user.context';
 
-import { CartProvider } from './contexts/cart.context';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-
-
-       
-
-          <CartProvider>
-            <App />
-          </CartProvider>
-
-
-        
-
-
+        <App />
       </BrowserRouter>
     </Provider>
 
